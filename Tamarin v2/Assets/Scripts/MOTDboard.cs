3d3a62ad392247863408adca3c5275a9068efde1
Cricket-Programming <liamlazaro@gmail.com>
2023-02-26 19:44:14 -0600

    Changed MOTD

--- Tamarin v2/Assets/Scripts/MOTDboard.cs
+++ Tamarin v2/Assets/Scripts/MOTDboard.cs
@@ -4,7 +4,8 @@ using TMPro;
 using System.Collections;
 public class MOTDboard : MonoBehaviour
 {
-    string textURL = "https://raw.githubusercontent.com/Cricket-Programming/Racoon-Message-of-the-Day/main/motd.txt";
+    //https://raw.githubusercontent.com/Cricket-Programming/Racoon/main/motd.txt?token=GHSAT0AAAAAAB6LLHZMGNWEMKHFPKPVNZ4AY74A6VA
+    public string textURL = ""; //I changed this to publicfrom samsams script
     public TMP_Text MOTD;
     // Start is called before the first frame update
     void Start()
