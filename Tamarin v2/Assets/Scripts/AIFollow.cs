1f40f30dba436eccdf7ddf5868ef20dc7d405968
Cricket-Programming <liamlazaro@gmail.com>
2023-03-08 20:17:37 -0600

    Horror enemy works!
    Horrified game for oddlers

--- Tamarin v2/Assets/Scripts/AIFollow.cs
+++ Tamarin v2/Assets/Scripts/AIFollow.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+
 public class AIFollow : MonoBehaviour
 {
     //CHANGE THE SPEED OF THE ENEMY IN THE NAVMESHAGENT COMPONENT
@@ -12,6 +13,9 @@ public class AIFollow : MonoBehaviour
 
     Vector3 newRandLocation;
 
+    void Start() {
+        StartCoroutine(wait());
+    }
     void Update()
     {
         float distance = Vector3.Distance(transform.position, Player.transform.position); //returns the distance between A and B, transform.position is the Enemy (basically the position of the player this script is part of)
@@ -22,30 +26,59 @@ public class AIFollow : MonoBehaviour
         }
         else 
         {
-            StartCoroutine(wait(10));
             
         }
     }
-    IEnumerator wait(float seconds)
+    IEnumerator wait()
     {
-        newRandLocation = new Vector3(Random.Range(-61, -30), Random.Range(0, ), Random.Range(0, ));
+        newRandLocation = new Vector3(Random.Range(-61, -30), 21, Random.Range(-2, 24));
         Debug.LogWarning(newRandLocation);
         Enemy.SetDestination(newRandLocation);
 
-        yield return new WaitForSeconds(seconds);
+        yield return new WaitForSeconds(3);
+        StartCoroutine(wait());
     }
 
 }
 
 /*
-    public NavMeshAgent enemy;
-    public Transform Player;
+    //CHANGE THE SPEED OF THE ENEMY IN THE NAVMESHAGENT COMPONENT
+    public NavMeshAgent Enemy;
+    public Transform Player; 
+    public Transform Floor;
+    public float EnemyRange = 4.0f; //changes the range of the enemy
 
+    Vector3 newRandLocation;
+
+    private void Start() {
+        StartCoroutine(wait());
+    }
     void Update()
     {
-        //if distance between transform.positions (declared with public Gameobject thing) is < something the set destintion)
-        enemy.SetDestination(Player.position);
+        float distance = Vector3.Distance(transform.position, Player.transform.position); //returns the distance between A and B, transform.position is the Enemy (basically the position of the player this script is part of)
+
+        if (distance < EnemyRange) //if the player is in range to the enemy, 
+        {
+            Enemy.SetDestination(Player.position);
+        }
+        else 
+        {
+            
+            
+        }
     }
+    IEnumerator wait()
+    {
+        newRandLocation = new Vector3(Random.Range(-61, -30), 21, Random.Range(-2, 24));
+        Debug.LogWarning(newRandLocation);
+        Enemy.SetDestination(newRandLocation);
+
+        yield return new WaitForSeconds(10);
+    }
+
+
+
+
 
 
 
