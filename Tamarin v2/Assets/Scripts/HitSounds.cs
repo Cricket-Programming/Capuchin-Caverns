08c47bf0816da49e1bbdb98e2e10c99264d81995
Cricket-Programming <liamlazaro@gmail.com>
2023-03-14 16:10:08 -0500

    Trying to add beeping /morecosmetics

--- Tamarin v2/Assets/Scripts/HitSounds.cs
+++ Tamarin v2/Assets/Scripts/HitSounds.cs
@@ -2,6 +2,7 @@ using System.Collections;
 //using System.Collections.Generic;
 using UnityEngine;
 
+//OBJECT BASED SCRIPT WORKING
 [RequireComponent(typeof(AudioSource))]
 public class HitSounds : MonoBehaviour
 {   
@@ -10,11 +11,13 @@ public class HitSounds : MonoBehaviour
     {
         Hitsound = GetComponent<AudioSource>();
     }
-    void OnCollisionEnter() //this is for the object the the gorilla player hit
+
+    void OnCollisionEnter() //this is for the object the the gorilla player hit if its the body 
     {
         Hitsound.Play();
     }
-    void OnTriggerEnter()  //this is for the gorilla player
+
+    void OnTriggerEnter()  //this is for the gorilla player's hands
     {
         Hitsound.Play();
     }
@@ -22,21 +25,20 @@ public class HitSounds : MonoBehaviour
 
     /*
 
-
-        public AudioSource Hitsound;
-    /*
-    void OnCollisionEnter(Collision other) {
-        if (other.CompareTag("WoodSound"))
+Broken I think but attach to player
+    public AudioSource Hitsound;
+    void OnCollisionEnter(Collision other) 
+    {
+        if (other.gameObject.CompareTag("WoodSound"))
         {
             Hitsound.Play();
         }
     }
-        void OnTriggerEnter(Collider other) {
-        if (other.CompareTag("WoodSound"))
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag("WoodSound"))
         {
             Hitsound.Play();
         }   
     } 
-    */
-
-
+*/
