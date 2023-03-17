626fdc737316552dd8b219f4896cd8bf7d0c1170
Cricket-Programming <liamlazaro@gmail.com>
2023-03-16 20:47:42 -0500

    HORROR TELEPORTATION WORKS
    HORROR TELEPORTATION IS ACTUALLY GOOD! LETS GO!!!
    Basically changed it so that it will teleport if you fall out of map. Most of the time though fluuffy will work will.
    Some sound additions
    Horror map changes
    forest changes

--- Tamarin v2/Assets/Scripts/TeleportWithDelay.cs
+++ Tamarin v2/Assets/Scripts/TeleportWithDelay.cs
@@ -14,11 +14,25 @@ public class TeleportWithDelay : MonoBehaviour
     public List<GameObject> targetObjects = new List<GameObject>();
 
     // Time to wait before teleporting (in seconds)
-    public float waitTimeBeforeTeleport = 1.0f;
+    public float waitTimeBeforeTeleport = .2f;
 
     // Time to wait after teleporting (in seconds)
-    public float waitTimeAfterTeleport = 1.0f;
+    public float waitTimeAfterTeleport = .2f;
 
+    void Update() {
+        if (objectToTeleport.transform.position.y < 15)
+        {
+            objectToDisable.SetActive(false);
+            objectToTeleport.transform.position = targetObjects[9].transform.position;
+            StartCoroutine(x());
+
+        }
+    }
+    IEnumerator x()
+    {
+        yield return new WaitForSeconds(0.2f);
+        objectToDisable.SetActive(true);
+    }
     public void OnTriggerEnter()
     {
         // Disable the object
@@ -28,7 +42,7 @@ public class TeleportWithDelay : MonoBehaviour
         StartCoroutine(WaitAndTeleport(waitTimeBeforeTeleport));      
     }
 
-    IEnumerator WaitAndTeleport(float waitTime)
+    IEnumerator WaitAndTeleport(float waitTime) //change this to IEnumerator
     {
         yield return new WaitForSeconds(waitTime);
         
@@ -36,21 +50,19 @@ public class TeleportWithDelay : MonoBehaviour
         {
             objectToTeleport.transform.position = obj.transform.position;
         }
-        //objectToTeleport.transform.position = targetObjects[4].transform.position;
-        //objectToTeleport.transform.position = targetObjects[5].transform.position;
-        // Wait for the specified time after teleporting
+
         yield return new WaitForSeconds(waitTimeAfterTeleport);
 
-        // Enable the object
         objectToDisable.SetActive(true);
            
     }
+
 }
 
 
 /*
-
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TeleportWithDelay : MonoBehaviour
@@ -62,65 +74,53 @@ public class TeleportWithDelay : MonoBehaviour
     public GameObject objectToTeleport;
 
     // Reference to the target object
-    
-    public GameObject targetObject1;
-    public GameObject targetObject2;
-    public GameObject targetObject3;
-    public GameObject targetObject4;
-    public GameObject targetObject5;
-    public GameObject targetObject6;
+    public List<GameObject> targetObjects = new List<GameObject>();
 
     // Time to wait before teleporting (in seconds)
-    public float waitTimeBeforeTeleport = 1.0f;
+    public float waitTimeBeforeTeleport = .2f;
 
     // Time to wait after teleporting (in seconds)
-    public float waitTimeAfterTeleport = 1.0f;
+    public float waitTimeAfterTeleport = .2f;
 
-    
-    //BoxCollider enemyBox;
-    void Start() 
-    {
-        //enemyBox = GetComponent<BoxCollider>();
+    bool dead = false;
+    void Update() {
+        if (objectToTeleport.transform.position.y < 15 && !dead)
+        {
+            dead = true;
+            StartCoroutine(WaitAndTeleport(waitTimeBeforeTeleport));
+
+        }
     }
-
     public void OnTriggerEnter()
     {
         // Disable the object
         objectToDisable.SetActive(false);
-        //enemyBox.enabled = false;
 
         // Wait for the specified time before teleporting
-        StartCoroutine(WaitAndTeleport(waitTimeBeforeTeleport));
-        
+        StartCoroutine(WaitAndTeleport(waitTimeBeforeTeleport));      
     }
 
-    IEnumerator WaitAndTeleport(float waitTime)
+    IEnumerator WaitAndTeleport(float waitTime) //change this to IEnumerator
     {
-        yield return new WaitForSeconds(waitTime);
-
-        // Teleport the object
-        //objectToTeleport.transform.position = new Vector3(objectToTeleport.transform.position.x, objectToTeleport.transform.position.y + 7f, objectToTeleport.transform.position.z); //y is the up direction
-        //objectToTeleport.transform.position += Vector3.up * Time.deltaTime;
+        //yield return new WaitForSeconds(waitTime);
         
-        objectToTeleport.transform.position = targetObject1.transform.position;
-        //yield return new WaitForSeconds(waitTimeAfterTeleport);
-        objectToTeleport.transform.position = targetObject2.transform.position;
-        //yield return new WaitForSeconds(waitTimeAfterTeleport);
-        objectToTeleport.transform.position = targetObject3.transform.position;
-        //yield return new WaitForSeconds(waitTimeAfterTeleport);
-        objectToTeleport.transform.position = targetObject4.transform.position;
-        //yield return new WaitForSeconds(waitTimeAfterTeleport);
-        objectToTeleport.transform.position = targetObject5.transform.position;
-        //yield return new WaitForSeconds(waitTimeAfterTeleport);
-        objectToTeleport.transform.position = targetObject6.transform.position;
-        
+        foreach (var obj in targetObjects)
+        {
+            objectToTeleport.transform.position = obj.transform.position;
+        }
+        //objectToTeleport.transform.position = targetObjects[4].transform.position;
+        //objectToTeleport.transform.position = targetObjects[5].transform.position;
         // Wait for the specified time after teleporting
-        yield return new WaitForSeconds(waitTimeAfterTeleport);
+        //yield return new WaitForSeconds(waitTimeAfterTeleport);
 
-        //enemyBox.enabled = true; 
         // Enable the object
         objectToDisable.SetActive(true);
+        dead = false;
            
+    }
+    void respawnPlayer()
+    {
+        
     }
 }
 
