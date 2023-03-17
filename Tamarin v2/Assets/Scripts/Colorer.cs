fb62b947bfe1ba60271139a510f999930e5a58b3
Cricket-Programming <liamlazaro@gmail.com>
2023-03-17 18:18:26 -0500

    Did i say player model works
    Added brown color
    Fixed Cosmetics

--- Tamarin v2/Assets/Scripts/Colorer.cs
+++ Tamarin v2/Assets/Scripts/Colorer.cs
@@ -5,22 +5,8 @@ using Photon.VR;
 public class Colorer : MonoBehaviour
 {
 public Color YourColor;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    void OnTriggerEnter()
-    {
-    Color myColour = YourColor;
-PhotonVRManager.SetColour(myColour);
-    
-    
+    void OnTriggerEnter() {
+        Color myColour = YourColor;
+        PhotonVRManager.SetColour(myColour);
     }
 }
