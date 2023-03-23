950bcde8a5ff91fc99abe5c46458f6bef8fbc89c
Cricket-Programming <liamlazaro@gmail.com>
2023-03-23 18:19:50 -0500

    fdsgkljdfjgkdfg

--- Tamarin v2/Assets/Scripts/Vibrations.cs
+++ Tamarin v2/Assets/Scripts/Vibrations.cs
@@ -5,22 +5,24 @@ using easyInputs;
 public class Vibrations : MonoBehaviour
 { 
     public float Amplitude;
-    public bool LeftHand; //if you create a bool, it makes it a checkbox I think
+    public bool LeftHand; //if you create a bool, it will show up as a checkbox in the editor
+    //public LayerMask vibrationEnabledLayers;
+    
 
+    void OnTriggerEnter(Collider other) {
+        if (!other.gameObject.CompareTag("WalkThrough")) {//does not let vibrations happen for objects with certain tags, should be replaced in the future with objects with certain layers.
+            if(LeftHand)
+            {
+                StartCoroutine(EasyInputs.Vibration(EasyHand.LeftHand, Amplitude, 0.15f));
+                Debug.Log("VIBRATED");
+                
+            }
+            else if(!LeftHand)
+            {
 
-    void OnTriggerEnter() {
+                StartCoroutine(EasyInputs.Vibration(EasyHand.RightHand, Amplitude, 0.15f));
 
-        if(LeftHand)
-        {
-            StartCoroutine(EasyInputs.Vibration(EasyHand.LeftHand, Amplitude, 0.15f));
-            
-        }
-        if(!LeftHand)
-        {
-
-            StartCoroutine(EasyInputs.Vibration(EasyHand.RightHand, Amplitude, 0.15f));
-
-
+            }
         }
 
     }
