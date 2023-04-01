d95b8244e9553e0410c31a388fb581b17b5e2d93
Cricket-Programming <liamlazaro@gmail.com>
2023-04-01 15:28:39 -0500

    The random points AI for Fluffy works.
    THe code in NetworkedEnemyFollow is fixed so that Fluffy will go to the random points if not chasing a player.
    NOT TESTED WITH OTHERS YET.

--- Tamarin v2/Assets/Scripts/NetworkedEnemyFollow.cs
+++ Tamarin v2/Assets/Scripts/NetworkedEnemyFollow.cs
@@ -7,37 +7,40 @@ using Photon.Pun;
 public class NetworkedEnemyFollow : MonoBehaviourPunCallbacks
 {
     [Header("Credit to FlimcyVR for portions of this script")]
+    //credit omarVision for some randomnavmeshstuff
     [Header("Set speed in navmeshagent component")]
     public float detectRange = 10f;
-
+    private NavMeshAgent nma = null; //nma stands for navMeshAgent
+    private Bounds bndFloor;
+    private Vector3 moveto;
+    private bool flag = false;
 
     private NavMeshAgent agent;
     private Transform target;
 
-    Vector3 newRandLocation;   
-    float sec = 0;
-
-
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
+        nma = this.GetComponent<NavMeshAgent>();
+        bndFloor = GameObject.Find("Horror Floor1").GetComponent<Renderer>().bounds;
+        SetRandomDestination();
+
         target = null;
-        StartCoroutine(wait());
     }
 
     void Update()
     {
-        
-        sec += Time.deltaTime;
-        if (PhotonNetwork.IsMasterClient)
+        if (nma.hasPath == false && flag == false)
         {
-            
+            flag = true;
+            SetRandomDestination();
+        }
+        if (PhotonNetwork.IsMasterClient)
+        {      
             if (target == null) //nobody is in the enemy's range
             {
                 
                 Collider[] colliders = Physics.OverlapSphere(transform.position, detectRange);
                 float closestDistance = Mathf.Infinity;
-
                 
                 foreach (Collider collider in colliders)
                 {
@@ -64,24 +67,23 @@ public class NetworkedEnemyFollow : MonoBehaviourPunCallbacks
                 {
                     target = null;
                 }
-                if (target.position.z < 15.5) { //makes sure the target is in the horror area.
-                    agent.SetDestination(target.position);
+                else if (target.position.z < 15.5) { //if target is in horror area, go to the horror area.
+                    nma.SetDestination(target.position);
                 }
                  //this piece of code comes after the  if (distanceToTarget > detectRange) so that if player get teleported, it will not go to the player's position anymore.
                 Debug.Log(target.position);
             }
         }
     }
-    IEnumerator wait()
+    private void SetRandomDestination()
     {
-        sec = 0;
-        newRandLocation = new Vector3(Random.Range(-61, -17), 21, Random.Range(-3, 24)); //this is where you put in the corners of the floor
-        //Debug.Log(newRandLocation); uncomment this to see the newRandLocation in the console
+        //1. pick a point
+        float rx = Random.Range(bndFloor.min.x, bndFloor.max.x);
+        float rz = Random.Range(bndFloor.min.z, bndFloor.max.z);
+        moveto = new Vector3(rx, this.transform.position.y, rz);
+        nma.SetDestination(moveto); //figure out path, starts gameobject moving
 
-        agent.SetDestination(newRandLocation);
-
-        yield return new WaitUntil( () =>  (Vector3.Distance(transform.position, newRandLocation) < 4 || Mathf.RoundToInt(sec) > 3) ); //&& Vector3.Distance(transform.position, Player.position) > EnemyRange);
-        StartCoroutine(wait());
+        flag = false;
     }
 }
 
@@ -97,9 +99,9 @@ using Photon.Pun;
 public class NetworkedEnemyFollow : MonoBehaviourPunCallbacks
 {
     [Header("Credit to FlimcyVR for portions of this script")]
+    //credit omarVision for some randomnavmeshstuff
     [Header("Set speed in navmeshagent component")]
     public float detectRange = 10f;
-    //due to networking, I think you cannot set the player position thing in the editor, instead you have to get the collider's parent, in this case gorillaplayer and check the position
 
 
     private NavMeshAgent agent;
@@ -178,74 +180,4 @@ public class NetworkedEnemyFollow : MonoBehaviourPunCallbacks
 
 
 
-
-
-
-
-
-
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.AI;
-using Photon.Pun;
-
-public class NetworkedEnemyFollow : MonoBehaviourPunCallbacks
-{
-    [Header("Credit to FlimcyVR for portions of this script")]
-    [Header("Set speed in navmeshagent component")]
-    public float detectRange = 10f;
-    public Transform Player;
-
-
-    private NavMeshAgent agent;
-    private Transform target;
-
-    void Start()
-    {
-        agent = GetComponent<NavMeshAgent>();
-        target = null;
-    }
-
-    void Update()
-    {
-        if (PhotonNetwork.IsMasterClient)
-        {
-            
-            if (target == null) //nobody is in the enemy's range
-            {
-                
-                Collider[] colliders = Physics.OverlapSphere(transform.position, detectRange);
-                float closestDistance = Mathf.Infinity;
-
-                
-                foreach (Collider collider in colliders)
-                {
-                    
-                    if (collider.gameObject.CompareTag("HandTag")) //checks if the collider is a player
-                    {
-                        Debug.Log("blah");
-                        float distance = Vector3.Distance(transform.position, collider.transform.position);
-
-                        if (distance < closestDistance)
-                        {
-                            closestDistance = distance;
-                            target = collider.transform;
-                        }
-                    }
-                }
-            }
-            else //makes enemy to go closest player 
-            {
-                agent.SetDestination(target.position);
-
-                float distanceToTarget = Vector3.Distance(transform.position, target.position);
-                if (distanceToTarget > detectRange) 
-                {
-                    target = null;
-                }
-            }
-        }
-    }
-}
 */
