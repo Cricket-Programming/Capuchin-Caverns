b5ef92926e57785fab61d735c0d79c15cec919cd
Cricket-Programming <liamlazaro@gmail.com>
2023-05-13 19:06:32 -0500

    IDK but it works
    OnlinePlayer counter a
    and the playfab id are broken totally won't work at all

--- KIPUNJI Project/Assets/Scripts/OnlinePlayerCounter.cs
+++ KIPUNJI Project/Assets/Scripts/OnlinePlayerCounter.cs
@@ -3,8 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using Photon.Pun;
+using Photon.Realtime;
 
-public class OnlinePlayerCounter : MonoBehaviour
+public class OnlinePlayerCounter : MonoBehaviourPunCallbacks
+{
+    private TMP_Text playerCountText;
+    private int playerCount;
+
+    void Start()
+    {
+        playerCountText = GetComponent<TMP_Text>();
+        UpdatePlayerCount();
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        UpdatePlayerCount();
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        UpdatePlayerCount();
+    }
+
+    private void UpdatePlayerCount()
+    {
+        playerCount = PhotonNetwork.PlayerList.Length;
+        playerCountText.text = playerCount.ToString();
+    }
+}
+
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Photon.Pun;
+
+//This scripts counts how many players total players there are online.
+public class OnlinePlayerCounter : MonoBehaviourPunCallbacks
 {
     //credits flimcy and pearvr
     private TMP_Text playerCountText;
@@ -20,7 +57,9 @@ public class OnlinePlayerCounter : MonoBehaviour
         if (PhotonNetwork.IsConnected)
         {
             playerCount = PhotonNetwork.CountOfPlayers;
+            Debug.Log(playerCount);
             playerCountText.text = playerCount.ToString();
         }
     }
 }
+*/
