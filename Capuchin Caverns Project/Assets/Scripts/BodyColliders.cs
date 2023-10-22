51f6281ae8e9af635c3bfaef946f29c461166e84
Cricket-Programming <liamlazaro@gmail.com>
2023-10-22 14:38:28 -0500

    Build 247 uploaded to applab
    body collider now follows polymorphism

--- Capuchin Caverns Project/Assets/Scripts/BodyColliders.cs
+++ Capuchin Caverns Project/Assets/Scripts/BodyColliders.cs
@@ -6,11 +6,11 @@ using Photon.Pun;
 // This script makes the body colliders and player name not show from the player's eyes. However, all of the other players will be able to see the player's name and collide with the body colliders.
 public class BodyColliders : MonoBehaviour
 { 
-    public PhotonView PhotonView;
+    [SerializeField] private PhotonView photonView;
 
     private void Update()
     {
-        if (PhotonView.IsMine){
+        if (photonView.IsMine){
             this.gameObject.SetActive(false);
         }
     }
