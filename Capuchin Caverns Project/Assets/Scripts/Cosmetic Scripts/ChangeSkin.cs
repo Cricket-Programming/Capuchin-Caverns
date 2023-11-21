2589d94932ab1bb8d64371b4be3b740e97ce691c
Cricket-Programming <liamlazaro@gmail.com>
2023-11-21 17:17:07 -0600

    skins works and are networked!

--- Capuchin Caverns Project/Assets/Scripts/Cosmetic Scripts/ChangeSkin.cs
+++ Capuchin Caverns Project/Assets/Scripts/Cosmetic Scripts/ChangeSkin.cs
@@ -3,49 +3,100 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using Photon.Pun;
-using Photon.VR.Player;
+using Photon.VR.Player; //for the type PhotonVRPlayer
 using Photon.VR; //for the type PhotonVRManager
+
 //each skin and cosmetic MUST have an original name
 //this script changes the material of the player. 
 public class ChangeSkin : MonoBehaviourPunCallbacks
 {
     [Tooltip("Set as null if you want to remove the skin.")]
     [SerializeField] private Material skin;
-    public PhotonVRManager myPhotonVRManager;
-    private GameObject myPlayer;
-    
+    private PhotonVRPlayer myPlayer;
+
     private Material previousMaterial;
-    //public override void OnJoinedRoom()
-    public void Update()
-    {
-        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
-        foreach (GameObject player in players)
-        {
-            if (player.GetComponent<PhotonView>().IsMine)
-            {
-                myPlayer = player;
-            }
-        }
-    }
     private void OnTriggerEnter(Collider other) {
         if (!other.CompareTag("HandTag")) return;
 
+        myPlayer = PhotonVRManager.Manager.LocalPlayer;
+
+        //photonView.RPC("setSkin", RpcTarget.AllBuffered);
+        // if (skin == null) { //remove skin
+        //     foreach (Renderer colourObject in myPlayer.ColourObjects) {
+        //         colourObject.material.mainTexture = null;
+        //         colourObject.material.color = PhotonVRManager.Manager.Colour; //usually skin.color is white
+        //     }        
+        // }  
+        // else {
+        //     foreach (Renderer colourObject in myPlayer.ColourObjects) {
+        //         colourObject.material.mainTexture = skin.mainTexture;
+        //         colourObject.material.color = skin.color; //usually skin.color is white
+                
+        //     }
+        // }  
         if (skin == null) { //remove skin
-            foreach (Renderer colourObject in myPlayer.GetComponent<PhotonVRPlayer>().ColourObjects) {
-                colourObject.material.mainTexture = null;
-                colourObject.material.color = myPhotonVRManager.Colour; //usually skin.color is white
-            }        
+            myPlayer.GetComponent<NetworkSkin>().RunRemoveNetworkSkin();
         }  
         else {
-            foreach (Renderer colourObject in myPlayer.GetComponent<PhotonVRPlayer>().ColourObjects) {
-                colourObject.material.mainTexture = skin.mainTexture;
-                colourObject.material.color = skin.color; //usually skin.color is white
-                
-            }
+            myPlayer.GetComponent<NetworkSkin>().RunSetNetworkSkin();
         }  
-        myPlayer.GetComponent<TagScript6>().initialMaterial = myPlayer.GetComponent<PhotonVRPlayer>().ColourObjects[0].material;
+
+        myPlayer.GetComponent<TagScript6>().initialMaterial = myPlayer.ColourObjects[0].material;
 
     }
-    
 
 }
+
+
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// using Photon.Pun;
+// using Photon.VR.Player;
+// using Photon.VR; //for the type PhotonVRManager
+// //each skin and cosmetic MUST have an original name
+// //this script changes the material of the player. 
+// public class ChangeSkin : MonoBehaviourPunCallbacks
+// {
+//     [Tooltip("Set as null if you want to remove the skin.")]
+//     [SerializeField] private Material skin;
+//     public PhotonVRManager myPhotonVRManager;
+//     private GameObject myPlayer;
+    
+//     private Material previousMaterial;
+//     //public override void OnJoinedRoom()
+//     public void Update()
+//     {
+//         // GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+//         // foreach (GameObject player in players)
+//         // {
+//         //     if (player.GetComponent<PhotonView>().IsMine)
+//         //     {
+//         //         myPlayer = player;
+//         //     }
+//         // }
+//         myPlayer = PhotonVRManager.Manager.LocalPlayer;
+//     }
+//     private void OnTriggerEnter(Collider other) {
+//         if (!other.CompareTag("HandTag")) return;
+
+//         if (skin == null) { //remove skin
+//             foreach (Renderer colourObject in myPlayer.GetComponent<PhotonVRPlayer>().ColourObjects) {
+//                 colourObject.material.mainTexture = null;
+//                 colourObject.material.color = myPhotonVRManager.Colour; //usually skin.color is white
+//             }        
+//         }  
+//         else {
+//             foreach (Renderer colourObject in myPlayer.GetComponent<PhotonVRPlayer>().ColourObjects) {
+//                 colourObject.material.mainTexture = skin.mainTexture;
+//                 colourObject.material.color = skin.color; //usually skin.color is white
+                
+//             }
+//         }  
+//         myPlayer.GetComponent<TagScript6>().initialMaterial = myPlayer.GetComponent<PhotonVRPlayer>().ColourObjects[0].material;
+
+//     }
+    
+
+// }
---
+++ Capuchin Caverns Project/Assets/Scripts/NetworkSkin.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+using Photon.VR;
+using Photon.VR.Player;
+public class NetworkSkin : MonoBehaviourPunCallbacks
+{
+    private List<MeshRenderer> ColourObjects = new List<MeshRenderer>();
+    private GameObject[] players;
+    [SerializeField] private Material skin;
+    private void Start() {
+        ColourObjects = GetComponent<PhotonVRPlayer>().ColourObjects;
+
+            
+        Invoke("newPlayerSkinCatchUp", 0.1f); 
+    }
+
+    private void newPlayerSkinCatchUp() {
+        players = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject player in players) {
+            Material playerMaterial = player.GetComponent<PhotonVRPlayer>().ColourObjects[0].material;
+            if (playerMaterial.mainTexture != null) {
+                if (skin.mainTexture.name.Equals(playerMaterial.mainTexture.name)) {   // + " (Instance)" {    
+                    player.GetComponent<NetworkSkin>().photonView.RPC("SetSkin", photonView.Owner);
+                }
+            }
+
+        }
+    }
+    // private void Update() {
+    //     Debug.Log(GetComponent<PhotonVRPlayer>().ColourObjects[0].material.mainTexture.name);
+    //     Debug.Log(skin.mainTexture.name);
+    // }
+    public void RunSetNetworkSkin() {
+        photonView.RPC("SetSkin", RpcTarget.All);
+    }
+    public void RunRemoveNetworkSkin() {
+        photonView.RPC("RemoveSkin", RpcTarget.All);
+    }
+    [PunRPC]
+    public void SetSkin() {
+        foreach (Renderer colourObject in ColourObjects) {
+            colourObject.material.mainTexture = skin.mainTexture;
+            colourObject.material.color = skin.color; //usually skin.color is white
+            
+        }
+    }
+    [PunRPC]
+    public void RemoveSkin() {
+        foreach (Renderer colourObject in ColourObjects) {
+            colourObject.material.mainTexture = null;
+            colourObject.material.color = PhotonVRManager.Manager.Colour; //usually skin.color is white
+        }   
+    }
+}
