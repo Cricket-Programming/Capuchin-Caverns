f1f20ffac4a85f5a4610df1c0e73711faaeda3e9
Cricket-Programming <liamlazaro@gmail.com>
2024-01-01 16:23:49 -0600

    More organization

--- Capuchin Caverns Project/Assets/Scripts/aaReadMe.cs
+++ Capuchin Caverns Project/Assets/Scripts/aaReadMe.cs
@@ -1,10 +1,10 @@
 /*
 THIS PROJECT'S CODING CONVENTIONS AND FORMATTING
 
-method names: pascalcase
-private variables - camelcase, no underscore prefixes.
-public variables - pascalcase
-protection modifiers: explicit even when private for everything.
+Method names: PascalCase
+Private variables - camelCase, no underscore prefixes.
+Public variables - PascalCase
+Protection modifiers: explicit even when private for everything, only make public when needs to be accessible somewhere and werite a comment that says where it is being used.
 
 For variables, include a lot of information and be explicit.
 Ex: [SerializeField] private float waitTime = 2f;
