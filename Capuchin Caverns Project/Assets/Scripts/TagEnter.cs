6864540ada1b28c3cd8c132258c71a257b6a5fe7
Cricket-Programming <liamlazaro@gmail.com>
2024-03-07 20:31:58 -0600

    Tag entrance put in

--- Capuchin Caverns Project/Assets/Scripts/TagEnter.cs
+++ Capuchin Caverns Project/Assets/Scripts/TagEnter.cs
@@ -6,7 +6,9 @@ using Photon.VR;
 // When the player passes through this object, it will let TagScript know that the player is in the tag area.
 public class TagEnter : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other) {
+    private void OnTriggerEnter() {
             PhotonVRManager.Manager.LocalPlayer.GetComponent<TagScript6>().ChangeIsInTagArea(true);
+            Debug.Log("Trigger enter");
+
     }
 }
