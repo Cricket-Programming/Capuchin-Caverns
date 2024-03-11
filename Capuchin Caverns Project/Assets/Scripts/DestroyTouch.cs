2763d4e0a0e2b622535a8732a0b1d16dfd9b12e9
Cricket-Programming <liamlazaro@gmail.com>
2024-03-10 21:51:42 -0500

    Made the easter egg cosmetics, jman cosmetics, and fixed bugs.

--- Capuchin Caverns Project/Assets/Scripts/DestroyTouch.cs
+++ Capuchin Caverns Project/Assets/Scripts/DestroyTouch.cs
@@ -5,12 +5,35 @@ using UnityEngine;
 public class DestroyTouch : MonoBehaviour
 {
     [SerializeField] private AudioSource sound;
+    [SerializeField] private float delay = 7f;
     private void OnTriggerEnter() {
         sound.Play();
         // Destroy the object after the sound finishes playing.
-        Invoke("DestroyObject", sound.clip.length); 
+        Invoke("HideObject", sound.clip.length); 
     }
-    private void DestroyObject() {
-        Destroy(gameObject);
+    private void HideObject() {
+        gameObject.SetActive(false);
+        Invoke("ShowObject", delay);
+    }
+    private void ShowObject() {
+        gameObject.SetActive(true);
     }
 }
+
+
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class DestroyTouch : MonoBehaviour
+// {
+//     [SerializeField] private AudioSource sound;
+//     private void OnTriggerEnter() {
+//         sound.Play();
+//         // Destroy the object after the sound finishes playing.
+//         Invoke("DestroyObject", sound.clip.length); 
+//     }
+//     private void DestroyObject() {
+//         Destroy(gameObject);
+//     }
+// }
