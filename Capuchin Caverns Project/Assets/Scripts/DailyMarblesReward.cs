53c940396c2bd4c9639ab7bee0c177c2b7deba0c
Cricket-Programming <liamlazaro@gmail.com>
2024-03-17 14:40:39 -0500

    Improved performance, sound plays when claiming marbles

--- Capuchin Caverns Project/Assets/Scripts/DailyMarblesReward.cs
+++ Capuchin Caverns Project/Assets/Scripts/DailyMarblesReward.cs
@@ -7,34 +7,48 @@ public class DailyMarblesReward : MonoBehaviour
 {
     [Tooltip("IE: Claimed!")]
     [SerializeField] private GameObject showAfter;
-    [SerializeField] private int howMuchADay = 100;     
+    [SerializeField] private int howMuchADay = 100;
+    [Tooltip("The sound to play when the user claims the currency.")]     
+    [SerializeField] private AudioSource soundToPlay;
     private string todayDate;
-    private bool hasEntered = false; // prevents user pressing button super fast and being able to get extra marbles
+    private bool hasEntered = false; // Prevents user pressing button super fast and being able to get extra marbles.
+
     private void Start() {
         todayDate = DateTime.Today.ToString("yyyy-MM-dd");
-        if (PlayerPrefs.GetInt("existingUser") == 0)
-        {
-            // If is a new user, set the date.
-            PlayerPrefs.SetString("previousDate", todayDate);  
-
-            PlayerPrefs.SetInt("existingUser", 1); 
-        }
 
         // It's the same day so deactivate the button.
-        if (PlayerPrefs.GetString("previousDate").Equals(todayDate)) {
-            RemovePurchaseButton();
-        }
+        // if (PlayerPrefs.GetString("previousDate").Equals(todayDate)) {
+        //     RemovePurchaseButton();
+        // }
     }
 
-    private void OnTriggerEnter() {
-        if (!hasEntered) {
-            hasEntered = true;
-            CurrencyManager.Instance.AddPlayFabCurrency(howMuchADay);
-            PlayerPrefs.SetString("previousDate", todayDate);
+    private void OnTriggerEnter(Collider other) {
+        if (!other.gameObject.CompareTag("HandTag")) return;
+        if (hasEntered) return;
+
+        hasEntered = true;
+        CurrencyManager.Instance.AddPlayFabCurrency(howMuchADay);
+        PlayerPrefs.SetString("previousDate", todayDate);
+
+        if (soundToPlay != null) {
+            // Hide the button temporarily so that it does not show, but scripts still work.
+            Renderer renderer = GetComponent<Renderer>();
+            renderer.enabled = false;
+            foreach (Transform child in transform)
+            {
+                // Hide the child GameObject
+                child.GetComponent<Renderer>().enabled = false;
+            }
+
+            soundToPlay.Play();
+            Invoke("RemovePurchaseButton", soundToPlay.clip.length); 
+        } 
+        else {
             RemovePurchaseButton();
         }
 
     }
+
     private void RemovePurchaseButton() {
         showAfter?.SetActive(true);
         Destroy(gameObject);
