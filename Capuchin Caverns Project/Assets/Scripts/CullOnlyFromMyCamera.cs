03b3d1e5873577c28f0ae766712b560879bf12f1
Cricket-Programming <liamlazaro@gmail.com>
2024-03-31 20:35:37 -0500

    Saving working version before adding new glass bridge.

--- Capuchin Caverns Project/Assets/Scripts/CullOnlyFromMyCamera.cs
+++ Capuchin Caverns Project/Assets/Scripts/CullOnlyFromMyCamera.cs
@@ -2,18 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
-//using Photon.VR;
+
 // On Culling mask - can be seen.
 // Not on culling mask - cannot be seen.
 // This script hides the gameobject with this script from this player's view. All the other players are able still see that gameobject.
-
+// My Camera will NOT see it.
+// Mirror camera will see it.
+// Other player's cameras will see it.
 // SET UP GUIDE
 // - Create a layer for gameobjects that are going to be hidden from the player's view. Put that in this script
 // - On the gorilla player's main camera, unselect that layer from the culling mask..
 // - Attack this script to the gameobjects which are going to be hidden from the player's view. 
 // - Fill in the photonView blank in the inspector.
 
-// Not sure if it actually works lol. It seems to at least for the nose.
+
 public class CullOnlyFromMyCamera : MonoBehaviour
 {
     [SerializeField] private PhotonView photonView;
