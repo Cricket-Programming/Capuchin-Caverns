474335ea5b39c156237bd438b7c72857742add9a
Cricket-Programming <liamlazaro@gmail.com>
2024-08-10 12:23:33 -0500

    Fixed the flashlight. The flashlight now works great.

--- Capuchin Caverns REVERTED URP/Assets/Scripts/FlashlightOnOff.cs
+++ Capuchin Caverns REVERTED URP/Assets/Scripts/FlashlightOnOff.cs
@@ -1,53 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using easyInputs;
-
-using Photon.VR;
-using Photon.Pun;
-using Photon.VR.Player;
-
-// This script turns on and off the flashlight when the player pushes the trigger button (index finger button) on their controller.
-// It may have an issue when multiple people have flashlight on, not syncing?
-public class FlashlightOnOff : MonoBehaviour
-{
-    [SerializeField] private bool isOn = true;
-    [SerializeField] private GameObject lightSource;
-    [SerializeField] private AudioSource clickSound;
-    [Tooltip("In seconds")]
-    [SerializeField] private float triggerCooldown = 0.5f;
-    [SerializeField] private EasyHand hand;
-    [SerializeField] private PhotonView photonView;
-    private float lastTriggerTime;
-
-    private void Update() {
-        if (!PhotonNetwork.InRoom) return;
-        if (!photonView.IsMine) return;
-        // Only turn on and off if the player has the cosmetic equiped.
-        if (!PhotonVRManager.Manager.LocalPlayer.GetComponent<PhotonVRPlayer>().Cosmetics.RightHand.Equals("Flashlight")) return;
-
-        if (CanTrigger()) {
-            if (hand == EasyHand.RightHand && EasyInputs.GetTriggerButtonDown(EasyHand.RightHand) || 
-                (hand == EasyHand.LeftHand && EasyInputs.GetTriggerButtonDown(EasyHand.LeftHand))) {
-                    ToggleFlashlight();
-                    lastTriggerTime = Time.time;
-                }
-        }
-    }
-
-    private bool CanTrigger() {
-        float timeElapsedSec = Time.time - lastTriggerTime; 
-        return timeElapsedSec >= triggerCooldown;
-    }
-
-    private void ToggleFlashlight() {
-        clickSound.Play();
-        lightSource.SetActive(!isOn);
-        isOn = !isOn;
-    }
-}
-
-
 // using System.Collections;
 // using System.Collections.Generic;
 // using UnityEngine;
@@ -56,35 +6,68 @@ public class FlashlightOnOff : MonoBehaviour
 // using Photon.VR;
 // using Photon.Pun;
 // using Photon.VR.Player;
+
+// // This script turns on and off the flashlight when the player pushes the trigger button (index finger button) on their controller.
+
+// public class FlashlightOnOff : MonoBehaviour
+// {
+//     [SerializeField] private GameObject lightSource;
+//     [SerializeField] private PhotonView photonView;
+
+//     private void ToggleFlashlight() {
+//         lightSource.SetActive(true);
+
+//     }
+// }
+
+
+// BROKEN, CODE FOR TURNING THE FLASHLIGHT ON AND OFF (DOES NOT WORK)
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using easyInputs;
+
+// using Photon.VR;
+// using Photon.Pun;
+// using Photon.VR.Player;
+
+// // This script turns on and off the flashlight when the player pushes the trigger button (index finger button) on their controller.
+// // It may have an issue when multiple people have flashlight on, not syncing?
 // public class FlashlightOnOff : MonoBehaviour
 // {
 //     [SerializeField] private bool isOn = true;
 //     [SerializeField] private GameObject lightSource;
 //     [SerializeField] private AudioSource clickSound;
-//     private bool canTrigger = true;
+//     [Tooltip("In seconds")]
+//     [SerializeField] private float triggerCooldown = 0.5f;
+//     [SerializeField] private EasyHand hand;
+//     [SerializeField] private PhotonView photonView;
+//     private float lastTriggerTime;
+
 //     private void Update() {
 //         if (!PhotonNetwork.InRoom) return;
-//         //only turn on and off if the player has it on.
+//         if (!photonView.IsMine) return;
+//         // Only turn on and off if the player has the cosmetic equiped.
 //         if (!PhotonVRManager.Manager.LocalPlayer.GetComponent<PhotonVRPlayer>().Cosmetics.RightHand.Equals("Flashlight")) return;
 
-//         if (EasyInputs.GetTriggerButtonDown(EasyHand.RightHand) && canTrigger) {
-//             if (isOn) {
-//                 //turnoff
-//                 clickSound.Play();
-//                 lightSource.SetActive(true);
-//                 isOn = false;
-//             }
-//             else {
-//                 // Turn on
-//                 clickSound.Play();
-//                 lightSource.SetActive(false);
-//                 isOn = true;
-//             }
-//             canTrigger = false;
+//         if (CanTrigger()) {
+//             if (hand == EasyHand.RightHand && EasyInputs.GetTriggerButtonDown(EasyHand.RightHand) || 
+//                 (hand == EasyHand.LeftHand && EasyInputs.GetTriggerButtonDown(EasyHand.LeftHand))) {
+//                     ToggleFlashlight();
+//                     lastTriggerTime = Time.time;
+//                 }
 //         }
-//         else {
-//             canTrigger = true;
-//         }
+//     }
 
+//     private bool CanTrigger() {
+//         float timeElapsedSec = Time.time - lastTriggerTime; 
+//         return timeElapsedSec >= triggerCooldown;
+//     }
+
+//     private void ToggleFlashlight() {
+//         clickSound.Play();
+//         lightSource.SetActive(!isOn);
+//         isOn = !isOn;
 //     }
 // }
+
