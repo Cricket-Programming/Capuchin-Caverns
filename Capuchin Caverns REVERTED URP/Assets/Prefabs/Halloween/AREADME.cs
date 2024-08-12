3eaf9d0b2c83db445aac0b8fcdfe0d5421813063
Cricket-Programming <liamlazaro@gmail.com>
2024-08-11 21:32:01 -0500

    All done for the day - halloween looking good.

---
+++ Capuchin Caverns REVERTED URP/Assets/Prefabs/Halloween/AREADME.cs
@@ -0,0 +1,51 @@
+/*
+
+This file contains information related to how to set up Halloween.
+In the hierarchy, their is a folder for Halloween. Enable it.
+Most folders contains a prefab of a Halloween prop. Decorate with those.
+For the environment, use the environment folder.
+
+LIGHTING
+For the environment to be dark, we need to change the AMBIENT LIGHT to DARK BLUE.
+1. Ambient light
+Ambient light is a default light, which illuminates all game objects in the scene evenly from all
+directions.
+I.e. Ambient light is used to adjust the overall look and feel of the environment with some
+specific color and brightness, so that we can create different moods
+Ex.
+Sunny day mood (bright orange),
+Dark night mood (black),
+Sad mood (light gray),
+Happy mood (light pink),
+Horror mood (dark blue), Etc.
+Adjusting Ambient light:
+Window Menu - Lighting - Scene Tab - Environment Lighting - Source - Color - Adjust the
+Ambient color
+
+Also, make sure to mark objects you want affected by ambient light to be marked non static. 
+Things you still want to be illuminated with baked lighting (like horror or the shop) should have static. 
+Ambient lighting only works with nonstatic gameobjects.
+
+COSMETICS
+Find what you need from the Halloween folder/drag and drop em in and create new ones.
+Ideas
+-Witch Hat
+Skeleton Skin
+-Glow in the Dark Sword
+-Witch Wand
+Shaving Cream Spray
+
+TODO
+Green+ Purple Lamps
+Jack o Lanterns (with candles inside)
+Green + Purple Lighting + Decorations to the Glass Bridge.
+Ghosts
+Coffin?
+
+
+
+
+
+
+
+*/
