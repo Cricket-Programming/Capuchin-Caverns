4de6fa5cb74a691e5995ec5537b054ef0ba01b65
Cricket-Programming <liamlazaro@gmail.com>
2024-10-14 14:57:13 -0500

    Save Halloween Cosmetics

--- Capuchin Caverns REVERTED URP/Assets/Scripts/DestroyTouch.cs
+++ Capuchin Caverns REVERTED URP/Assets/Scripts/DestroyTouch.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//When the player touches the object, it will disappear. It will respawn after respawnDelay.
+
 public class DestroyTouch : MonoBehaviour
 {
     [SerializeField] private AudioSource sound;
-    [SerializeField] private float delay = 7f;
+    [SerializeField] private float respawnDelay = 7f;
     private void OnTriggerEnter() {
         sound.Play();
         // Destroy the object after the sound finishes playing.
@@ -13,27 +15,9 @@ public class DestroyTouch : MonoBehaviour
     }
     private void HideObject() {
         gameObject.SetActive(false);
-        Invoke("ShowObject", delay);
+        Invoke("ShowObject", respawnDelay);
     }
     private void ShowObject() {
         gameObject.SetActive(true);
     }
 }
-
-
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-
-// public class DestroyTouch : MonoBehaviour
-// {
-//     [SerializeField] private AudioSource sound;
-//     private void OnTriggerEnter() {
-//         sound.Play();
-//         // Destroy the object after the sound finishes playing.
-//         Invoke("DestroyObject", sound.clip.length); 
-//     }
-//     private void DestroyObject() {
-//         Destroy(gameObject);
-//     }
-// }
